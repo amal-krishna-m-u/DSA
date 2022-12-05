4dbc12465890b16ede29326030a7c1e444ed9ce1
amal-krishna-m-u <amalkrishnam3@gmail.com>
2022-12-06 00:40:42 +0530

    insertion sort

---
+++ inser_sort.c
@@ -0,0 +1,30 @@
+//this is program to sort the array using insertion sort
+#include<stdio.h>
+int main()
+{
+int a[10],i,j,n,temp;
+printf("enter the size of array");
+scanf("%d",&n);
+printf("enter the elements of array");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<n;i++)
+{
+temp=a[i];
+j=i-1;
+while((temp<a[j])&&(j>=0))
+{
+a[j+1]=a[j];
+j=j-1;
+}
+a[j+1]=temp;
+}
+printf("sorted array is");
+for(i=0;i<n;i++)
+{
+printf(" %d ",a[i]);
+}
+return 0;
+}
